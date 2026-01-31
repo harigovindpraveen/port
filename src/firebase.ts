610af3081658200e5165e977ee236ec430bba9cf
harigovindpraveen <harigovind.praveen@gmail.com>
2026-01-31 18:01:00 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,1 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  // COPY THIS FROM YOUR FIREBASE CONSOLE
+  apiKey: "AIzaSy...",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
